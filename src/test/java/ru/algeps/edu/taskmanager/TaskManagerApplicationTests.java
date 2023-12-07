81c691c072fb6ebce19c66cc57d787b4239849d4
Nikita Vorobev <nik17052001@outlook.com>
2023-12-08 00:16:34 +0600

    init

---
+++ src/test/java/ru/algeps/edu/taskmanager/TaskManagerApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.algeps.edu.taskmanager;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+  @Test
+  void contextLoads() {
+    assertTrue(true);
+  }
+}
